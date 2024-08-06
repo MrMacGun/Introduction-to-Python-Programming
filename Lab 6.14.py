23b4928154cb4975cd4f7f1476994c05c7a00515
MrMacGun <133312820+MrMacGun@users.noreply.github.com>
2024-08-06 00:45:05 -0400

    corrected error

--- Lab 6.14.py
+++ Lab 6.14.py
@@ -3,25 +3,11 @@
 #I.E. Input 6 output is 011
 #input must be greater than 0, output as x mod 2 and then assign x with x divided by 2
 
-value = input(int())
-if value <= 0:
-        raise ValueError("Input must be a positive integer")
+num1 = int(6)
 
-    binary_digits = []
-    while value > 0:
-        remainder = value % 2
-        binary_digits.append(str(remainder))
-        value //= 2
+while num1 > 0:
+    print(num1 % 2, end = '')
+    num1 = num1//2
 
-    # Join the list of binary digits and reverse the string
-    value = ''.join(binary_digits)[::-1]
-    return value
-
-# Example usage:
-try:
-    num = int(input("Enter a positive integer: "))
-    reversed_binary = reverse_binary(num)
-    print(f"Reverse binary representation: {reversed_binary}")
-except ValueError as e:
-    print(e)
+print("")
 
