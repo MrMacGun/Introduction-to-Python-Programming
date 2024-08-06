cee8821683d8dccf10a164ba09df6f295ddc6c5b
MrMacGun <133312820+MrMacGun@users.noreply.github.com>
2024-08-05 23:53:40 -0400

    Corrected Code

--- Lab 6.16.py
+++ Lab 6.16.py
@@ -3,31 +3,23 @@
 # Lab reference https://learn.zybooks.com/zybook/WGUD335Pythonv1/chapter/6/section/16
 
 #''' Read in first equation, ax + by = c '''
-a = int(input())
-b = int(input())
-c = int(input())
+a = int(8)
+b = int(7)
+c = int(38)
 
 #''' Read in second equation, dx + ey = f '''
-d = int(input())
-e = int(input())
-f = int(input())
+d = int(3)
+e = int(-5)
+f = int(-1)
 
-def function_main(a, b, c, d, e, f):
-    solution = False
+# Initialize solution variables
+check = False
 
-    for x in range(-10, 11):
-        for y in range(-10, 11):
+for x in range(-10, 10, 1):
+    for y in range(-10, 10, 1):
+        if a * x + b * y == c and d * x + e * y == f:
+            check = True
+            print(f"x = {x} , y = {y}")
 
-            if (a*x+b*y) == c and (d*x+e*y) == f:
-                solution = True
-                print(f"x = {x}, y = {y}")
-                break
-
-        if solution:
-            False
-            print("There is no solution")
-            break
-
-    if not solution:
-        print("There is no solution")
-
+if check == False:
+    print('There is no solution')
