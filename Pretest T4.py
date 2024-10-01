8e120abf7138a54c1a5023f8deba74fd2ccc9eb7
MrMacGun <133312820+MrMacGun@users.noreply.github.com>
2024-09-30 22:43:33 -0400

    Update Pretest T4.py

--- Pretest T4.py
+++ Pretest T4.py
@@ -1,9 +1,9 @@
 #Program takes three integers to output the area of a Trapezoid
 
-Base1 = int(input("Enter Base 1 of the Trapazoid: "))
-Base2 = int(input("Enter Base 2 of the Trapazoid: "))
-Height = int(input("Enter the Height of the trapazoid: "))
+Base1 = float(input())
+Base2 = float(input())
+Height = float(input())
 
 Area = ((Base1 * Base2 / 2)) * Height
 
-print(f'Trapezoid area: {Area} square meters')
+print(f'Trapezoid area: {Area:.1f} square meters')
