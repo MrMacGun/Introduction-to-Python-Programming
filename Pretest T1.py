5972eb44b40b26ed2c31ca2d9466ff76633ac8ff
MrMacGun <133312820+MrMacGun@users.noreply.github.com>
2024-09-30 22:33:55 -0400

    Update Pretest T1.py

--- Pretest T1.py
+++ Pretest T1.py
@@ -3,9 +3,9 @@
 #Input 1 = Employee A * 1 etc.
 #All inputs are then combined as a total then printed as a single input
 
-A_input = int(input('Enter the number of trips Employee A travled'))
-B_input = int(input('Enter the number of trips Employee B travled'))
-C_input = int(input('Enter the number of trips Employee C travled'))
+A_input = int(input())
+B_input = int(input())
+C_input = int(input())
 
 EmployeeA = float(15.62)
 EmployeeB = float(41.85)
@@ -15,4 +15,4 @@ TotalMiles = ((A_input * EmployeeA) +
               (B_input * EmployeeB) + 
               (C_input * EmployeeC))
 
-print(f'Distance: {TotalMiles} miles')
+print(f'Distance: {TotalMiles:.2f} miles')
