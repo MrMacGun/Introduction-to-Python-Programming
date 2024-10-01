d78fa98462fac2bb89ae129e5287a442c90017e3
MrMacGun <133312820+MrMacGun@users.noreply.github.com>
2024-09-30 22:36:12 -0400

    Update Pretest T2.py

--- Pretest T2.py
+++ Pretest T2.py
@@ -2,7 +2,7 @@
 #Ounces are then divided up via tons, pounds and lastly onces 
 #If the input is 32035 then the output is Tones: 1, Pounds: 2, Ounces: 3
 
-user_int = int(input("Input Ounces: "))
+user_int = int(input())
 
 tons = user_int // 32000  # 1 ton = 32,000 ounces
 remaining_ounces = user_int % 32000
@@ -10,7 +10,9 @@ remaining_ounces = user_int % 32000
 pounds = remaining_ounces // 16  # 1 pound = 16 ounces
 ounces = remaining_ounces % 16
 
-print(f'Tons: {tons}, Pounds: {pounds}, Ounces: {ounces}')
+print(f'Tons: {tons}')
+print(f'Pounds: {pounds}') 
+print(f'Ounces: {ounces}')
         
 
 
